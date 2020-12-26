4a605ec287b7c573314a0ff1b2e4a04ce2b4942c
Koji Arai <jca02266@gmail.com>
2020-12-26 15:31:39 +0900

    Allow Traditional Bourne Shell

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ rm -rf ./dist
 mkdir -p dist/libs
 cp -a node_modules/lz-string/libs/lz-string.min.js dist/libs
 
-if [[ $(git symbolic-ref -q HEAD --short) -eq main ]]; then
+if [ x`git symbolic-ref -q HEAD --short` = xmain ]; then
   url="https://flingcode.netlify.app"
 else
   url="https://test-flingcode.netlify.app"
